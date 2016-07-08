bad36f018fbba7caaa213eb180e7144a34d8ea41
RubabSyed7 <rubab.syed@7vals.com>
2016-07-08 20:20:32 +0500

    header added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
+  resources :questions
+  devise_for :users, controllers: {
+               registrations: 'users/registrations'
+             }
+#  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
